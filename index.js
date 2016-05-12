1d619a67a5a73eb652a138606fcd6614ef2029c9
Collective Cloud Peru <collectivecloudperu@gmail.com>
2016-05-11 22:05:02 -0500

    Cambios

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 3000))
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
