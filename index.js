c233675b8d18130ea5325e5f23f8e5be8de62827
Juan Ricardo Castro Lurita <jrclurita@gmail.com>
2016-07-25 10:17:18 -0500

    Cambios

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
