3cd3feac9ec69d34288e84d2f46006276b62fa59
Juan Ricardo Castro Lurita <jrclurita@gmail.com>
2016-07-25 11:42:44 -0500

    Cambios

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function newResponse(recipientId, text) {
 
         //sendMessage(recipientId, message);
         if(xtxt != null) {
-            query = txt;
+            query = text;
         } 
         sendButtonMessage(recipientId, query);
         return true
